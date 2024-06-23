4c1a589365abfde48d5c2236693e5fd9a5c8cfb7
innightwolfsleep <innightwolfsleep@yandex.ru>
2024-06-23 07:22:49 +0600

    add chars for file name excluse

--- get_user_data.py
+++ get_user_data.py
@@ -47,7 +47,6 @@ def parseHar():
             url_contacts = url_data.group(0)
 
     request_data = RequestData(reqId=reqId, aimsid=aimsid)
-    print(request_data, url_contacts)
     user_config = UserConfig(request_data=request_data, cookies=cookies, patchVersion=patchVersion,
                              url_contacts=url_contacts)
 
