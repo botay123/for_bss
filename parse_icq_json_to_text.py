b4ab5680d396d5932582866897f88ad60fd7a690
innightwolfsleep <innightwolfsleep@yandex.ru>
2024-06-23 15:53:10 +0600

    fixed few parsing errors

--- parse_icq_json_to_text.py
+++ parse_icq_json_to_text.py
@@ -6,35 +6,37 @@ import datetime
 
 
 def parse(fname: str, chatinfo: str):
-    history = []
+    history = {}
     users = {}
 
     with open(chatinfo, 'r', encoding="utf-8") as f:
         try:
             user_info = json.load(f)
-            for s in user_info["results"]["members"]:
-                if "friendly" in s and "sn" in s:
-                    users.update({s["sn"]: s["friendly"]})
+            if "members" in user_info["results"]:
+                for s in user_info["results"]["members"]:
+                    if "friendly" in s and "sn" in s:
+                        users.update({s["sn"]: s["friendly"]})
         except Exception as ex:
-            print("error while parsing " + chatinfo, ex)
+            print("error while parsing " + chatinfo, ex, user_info)
 
     with open(fname, 'r', encoding="utf-8") as f:
         raw_history = json.load(f)
-        print(raw_history)
 
     for s in raw_history:
-        if "text" in s and "chat" in s and "time" in s:
+        if "text" in s and "time" in s:
             #            history.append({"sender": users[s["chat"]["sender"]],
             #                            "time": str(datetime.datetime.fromtimestamp(s["time"])),
             #                            "text": s["text"]})
-            if s["chat"]["sender"] in users:
-                sender = users[s["chat"]["sender"]]
+            if "chat" in s:
+                if s["chat"]["sender"] in users:
+                    sender = users[s["chat"]["sender"]]
+                else:
+                    sender = s["chat"]["sender"]
             else:
-                sender = s["chat"]["sender"]
+                sender = "-"
             time = str(datetime.datetime.fromtimestamp(s["time"]))
             text = s["text"]
-            history.append(sender + " (" + time + "):" + text)
-    history.reverse()
+            history.update({s["time"]: sender + " (" + time + "):" + text})
 #    try:
 #        with open(chat_name.replace("/", "") + "_history.txt", "w", encoding="utf-8") as f:
 #            f.write("\n".join(history))
@@ -47,15 +49,23 @@ def parse(fname: str, chatinfo: str):
 
 if __name__ == '__main__':
     results = os.listdir("results")
+
     if not os.path.exists("parsed_results"):
         os.mkdir("parsed_results")
+
     for chat in results:
         jsons = os.listdir(os.path.join("results", chat, "json"))
         chatinfo = os.path.join("results", chat, "chatInfo.json")
-        parsed_text = []
+        parsed_dict = {}
+
         for json_part in jsons:
             f = os.path.join("results", chat, "json", json_part)
-            p = parse(f, chatinfo)
-            parsed_text.extend(p)
+            parsed_dict.update(parse(f, chatinfo))
+
+        timestamps = parsed_dict.keys()
+        timestamps = list(timestamps)
+        timestamps.sort()
+
         with open(os.path.join("parsed_results", chat + "_history.txt"), "w", encoding="utf-8") as f:
-            f.write("\n".join(parsed_text))
+            for t in timestamps:
+                f.write("\n" + parsed_dict[t])
